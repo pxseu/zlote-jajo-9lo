1264001afe4d7c5e299f2772a828581fc3bd9739
Kuba Ellwart <57842793+pxseu@users.noreply.github.com>
2022-04-11 10:32:57 +0200

    remove: dist

--- packages/api/dist/index.js
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-import { router } from "./routes/index.js";
-import { PORT } from "./utils/config.js";
-const app = Express();
-app.set("trust proxy", 1);
-app.use(router);
-await new Promise((resolve) => app.listen(Number(PORT), () => resolve(null)));
-console.log(`Listening on port ${PORT}`);
--- packages/api/dist/utils/config.js
+++
@@ -1,2 +0,0 @@
-export const DEV_MODE = process.env.NODE_ENV === "development";
-export const PORT = process.env.PORT || "8081";
