b119affde54aef6cf9b77295792182d8a42c052e
Kuba Ellwart <57842793+pxseu@users.noreply.github.com>
2022-04-11 10:28:15 +0200

    feat: kys5

--- packages/api/src/data/dane.ts
+++ packages/api/src/data/dane.ts
@@ -1,30 +1,49 @@
 export interface Question {
-	text: string;
-	id: string;
-	answers: { id: string; text: string }[];
+  id: string;
+  questions: {
+    id: number;
+    text: string;
+    correct: string;
+    answers:
+      | {
+          id: string;
+          text: string;
+        }[]
+      | null;
+  }[];
 }
 
 export const DANE: Question[] = [
-	{
-		id: "6eb3491d84e2db9743f241b6719f0b30c9e",
-		text: "What is your favorite color?",
-		answers: [
-			{
-				id: "A",
-				text: "Red",
-			},
-			{
-				id: "B",
-				text: "Blue",
-			},
-			{
-				id: "C",
-				text: "Green",
-			},
-			{
-				id: "D",
-				text: "Yellow",
-			},
-		],
-	},
+  {
+    id: "fMLktCCH5xN9grgzMzuZ2srqxdebkd",
+    questions: [
+      {
+        id: 1,
+        text: "Kiedy struny dźwięk wydają,\nWargi, język - już czekają,\nOne tworzą z dźwięków słowa,\nTak powstaje nasza...",
+        answers: null,
+        correct: "mowa.",
+      },
+    ],
+  },
+  {
+    id: "mffmM2fCPWScgq78ZCS94vbkm8dtnK",
+    questions: [
+      {
+        id: 1,
+        text: "Jak nazywa się dawna siedziba królów Polski?",
+        correct: "A",
+        answers: [
+          { id: "A", text: "Wawel" },
+          {
+            id: "B",
+            text: "Zamościu",
+          },
+          {
+            id: "C",
+            text: "",
+          },
+        ],
+      },
+    ],
+  },
 ];
